004b19d6fd8969979e18ad3217a0110aa6adfe3a
WchenJ <90740353+WchenJ@users.noreply.github.com>
2022-03-09 02:28:15 +0800

    Create test.py

---
+++ sql/test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 10:53:19 2022
+
+@author: bobga
+"""
+
+import sqlite3
+class change:
+  def __init__(self):
+        self.page = 1
+        self.pages = []
+        self.enable = False
+        self.conn = None
+        self.cursor = None
+change.conn = sqlite3.connect('my1.db')
+change.cursor = change.conn.cursor()
+change.cursor.execute("UPDATE Healthdata SET tempreture = 36.6 WHERE ID = 2;")
+change.cursor.execute("UPDATE Healthdata SET time = '6.MAR.2022' WHERE ID = 2;")
+change.conn.commit()
+change.cursor.execute(
+        "select * from Healthdata"
+        )
+print(change.cursor.fetchall())
+change.conn.close()

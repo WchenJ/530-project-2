9d41db1ff72611f5acb886ce659a5d2cd9cb582d
WchenJ <90740353+WchenJ@users.noreply.github.com>
2022-03-14 04:05:56 +0800

    Add files via upload

--- Djtest/Djtest/urls.py
+++ Djtest/Djtest/urls.py
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from test0 import views
+from django.conf.urls import url, include
+from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+from test0.views import UserViews,HealthdataViews,DeviceViews,DivrecViews,PermissionViews
+
+router.register(r'User', UserViews, basename='User') 
+router.register(r'Healthdata', HealthdataViews, basename='Healthadata') 
+router.register(r'Device', HealthdataViews, basename='Device') 
+router.register(r'Divrec', HealthdataViews, basename='Divrec') 
+router.register(r'Permission', HealthdataViews, basename='Permission') 
 urlpatterns = [
    # path('admin/', admin.site.urls),
    path('index/',views.index),
@@ -26,5 +38,7 @@ urlpatterns = [
    path('user/', views.UsersView.as_view()),
    path('user/<int:pk>/', views.UsersView.as_view()),
    path('data/', views.HealthdataView.as_view()),
-   path('data/<int:pk>/', views.HealthdataView.as_view())
+   path('data/<int:pk>/', views.HealthdataView.as_view()),
+   url('/', include(router.urls)),
+   path('api-auth/', include('rest_framework.urls', namespace='rest_framework')) 
 ]
